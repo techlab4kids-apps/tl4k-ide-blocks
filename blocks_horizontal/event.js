45ac271255506cb9448a25550eb91f576055ca53
JeremyGamer13 <jeremygtz753@gmail.com>
2022-10-29 10:52:57 -0700

    blue flag event when?

--- blocks_horizontal/event.js
+++ blocks_horizontal/event.js
@@ -35,14 +35,14 @@ Blockly.Blocks['event_whenflagclicked'] = {
    * Block for when flag clicked.
    * @this Blockly.Block
    */
-  init: function() {
+  init: function () {
     this.jsonInit({
       "id": "event_whenflagclicked",
       "message0": "%1",
       "args0": [
         {
           "type": "field_image",
-          "src": Blockly.mainWorkspace.options.pathToMedia + "icons/event_whenflagclicked.svg",
+          "src": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABgAAAAYCAYAAADgdz34AAAABmJLR0QA/wD/AP+gvaeTAAACJ0lEQVRIid2Uy09TQRSHv2kv1VR5hhQQqwlYGtqCmtbHokhDTNQtEVz4B/gfmBCDXoyGmLj3kbBwY2LY6MpHWJDUoBukKsTKQ6I2paWGBipCo73jBii30BpI78ZfMpkzMznznTOPAwZLbFgt/ZWY011k9gzysSdZLICStdJXkPRjSns41hcH2Y7EjmAVmESKFyjyCaPqr50AzBtWbeAsEKDE0oy7w4rnfAXNbWU0nlY40CQwmX0kY73UBr4QGw5vu5vndg31Zy6wv3OGhecZfQbrajk3Rp2zXRdCqe0Irg5o8k8w+uwWR9VLmE13eaeNgarRqtoQ4jKKdpUy+wLy+3Hg2vaAQlIsbk51ZUjNJ/n8egBv5CBan2Bf5TL1nlkafGkWYz8YeVy94bIjwHpOpTY/vs7Nc1WAHQCNr5sXTLsA/EOaNBagabph8QEyYzAA3QkZkYGeUOAVyTlWUt/I/M5gLa/GpDjYXFryuumPaCsgMWsl+ilEfLoOSQTBClBBuW2GExcXsVi9hQFSF8RWQGT8MNCLwzXAYHc2nNabbQzdu4/TH6ThpBMhbDmef1iKhRgf8iHFo/wAeEpIfUgoZ/bD9SAB1Us42MPUiJOaxhBVh5YwKYKfib3MhZtYXS5B0sl79VUhQH4Nq6vADbzqHaJTbUQn3QjKECKOlG8Iqblh7apUsFayX661gjLgH/zPgPm1PlFMQPaSHa4HTIcnSFW8LSbAcP0F3uGqEimnx6MAAAAASUVORK5CYII=", // Blockly.mainWorkspace.options.pathToMedia + "icons/event_whenflagclicked.svg",
           "width": 40,
           "height": 40,
           "alt": "When green flag clicked",
@@ -64,27 +64,39 @@ Blockly.Blocks['dropdown_whenbroadcast'] = {
    * Block for when broadcast dropdown (used for shadow).
    * @this Blockly.Block
    */
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldIconMenu(
-            [
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_blue.svg',
-                value: 'blue', width: 48, height: 48, alt: 'Blue'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_green.svg',
-                value: 'green', width: 48, height: 48, alt: 'Green'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_coral.svg',
-                value: 'coral', width: 48, height: 48, alt: 'Coral'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_magenta.svg',
-                value: 'magenta', width: 48, height: 48, alt: 'Magenta'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_orange.svg',
-                value: 'orange', width: 48, height: 48, alt: 'Orange'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_purple.svg',
-                value: 'purple', width: 48, height: 48, alt: 'Purple'}
-            ]), 'CHOICE');
+      .appendField(new Blockly.FieldIconMenu(
+        [
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_blue.svg',
+            value: 'blue', width: 48, height: 48, alt: 'Blue'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_green.svg',
+            value: 'green', width: 48, height: 48, alt: 'Green'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_coral.svg',
+            value: 'coral', width: 48, height: 48, alt: 'Coral'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_magenta.svg',
+            value: 'magenta', width: 48, height: 48, alt: 'Magenta'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_orange.svg',
+            value: 'orange', width: 48, height: 48, alt: 'Orange'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_when-broadcast-received_purple.svg',
+            value: 'purple', width: 48, height: 48, alt: 'Purple'
+          }
+        ]), 'CHOICE');
     this.setOutput(true);
     this.setColour(Blockly.Colours.event.primary,
-        Blockly.Colours.event.secondary,
-        Blockly.Colours.event.tertiary
+      Blockly.Colours.event.secondary,
+      Blockly.Colours.event.tertiary
     );
   }
 };
@@ -94,7 +106,7 @@ Blockly.Blocks['event_whenbroadcastreceived'] = {
    * Block for when broadcast received.
    * @this Blockly.Block
    */
-  init: function() {
+  init: function () {
     this.jsonInit({
       "id": "event_whenbroadcastreceived",
       "message0": "%1 %2",
@@ -126,27 +138,39 @@ Blockly.Blocks['dropdown_broadcast'] = {
    * Block for broadcast dropdown (used for shadow).
    * @this Blockly.Block
    */
-  init: function() {
+  init: function () {
     this.appendDummyInput()
-        .appendField(new Blockly.FieldIconMenu(
-            [
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_blue.svg',
-                value: 'blue', width: 48, height: 48, alt: 'Blue'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_green.svg',
-                value: 'green', width: 48, height: 48, alt: 'Green'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_coral.svg',
-                value: 'coral', width: 48, height: 48, alt: 'Coral'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_magenta.svg',
-                value: 'magenta', width: 48, height: 48, alt: 'Magenta'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_orange.svg',
-                value: 'orange', width: 48, height: 48, alt: 'Orange'},
-              {src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_purple.svg',
-                value: 'purple', width: 48, height: 48, alt: 'Purple'}
-            ]), 'CHOICE');
+      .appendField(new Blockly.FieldIconMenu(
+        [
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_blue.svg',
+            value: 'blue', width: 48, height: 48, alt: 'Blue'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_green.svg',
+            value: 'green', width: 48, height: 48, alt: 'Green'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_coral.svg',
+            value: 'coral', width: 48, height: 48, alt: 'Coral'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_magenta.svg',
+            value: 'magenta', width: 48, height: 48, alt: 'Magenta'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_orange.svg',
+            value: 'orange', width: 48, height: 48, alt: 'Orange'
+          },
+          {
+            src: Blockly.mainWorkspace.options.pathToMedia + 'icons/event_broadcast_purple.svg',
+            value: 'purple', width: 48, height: 48, alt: 'Purple'
+          }
+        ]), 'CHOICE');
     this.setOutput(true);
     this.setColour(Blockly.Colours.event.primary,
-        Blockly.Colours.event.secondary,
-        Blockly.Colours.event.tertiary
+      Blockly.Colours.event.secondary,
+      Blockly.Colours.event.tertiary
     );
   }
 };
@@ -156,7 +180,7 @@ Blockly.Blocks['event_broadcast'] = {
    * Block to send a broadcast.
    * @this Blockly.Block
    */
-  init: function() {
+  init: function () {
     this.jsonInit({
       "id": "event_broadcast",
       "message0": "%1 %2",
