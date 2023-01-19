0591efe7129c0136be4c80427b52a7d6d5bd93f4
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-18 22:44:11 -0800

    Update field_colour_slider.js

--- core/field_colour_slider.js
+++ core/field_colour_slider.js
@@ -160,7 +160,7 @@ Blockly.FieldColourSlider.prototype.createColourStops_ = function(channel) {
 Blockly.FieldColourSlider.prototype.setGradient_ = function(node, channel) {
   var gradient = this.createColourStops_(channel).join(',');
   goog.style.setStyle(node, 'background-image',
-      'url("static/assets/482dc5011057fe26e9542e9476601bf2.png")');
+      'linear-gradient(left, ' + gradient + '), url("static/assets/482dc5011057fe26e9542e9476601bf2.png")');
   goog.style.setStyle(node, 'background',
       '-moz-linear-gradient(left, ' + gradient + ')');
   goog.style.setStyle(node, 'background',
