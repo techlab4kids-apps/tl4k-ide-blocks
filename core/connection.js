a9e87352d0abcea02dd846caefc3378ededab699
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-01-23 23:22:56 -0800

    removed potentialy usless hack

--- core/connection.js
+++ core/connection.js
@@ -312,7 +312,7 @@ Blockly.Connection.prototype.canConnectWithReason_ = function(target) {
     return Blockly.Connection.REASON_CHECKS_FAILED;
   } else if (blockA.isShadow() && !blockB.isShadow()) {
     return Blockly.Connection.REASON_SHADOW_PARENT;
-  } else if ((blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE &&
+  } /*else if ((blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE &&
       blockB.type != Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
       superiorConn == blockA.getInput('custom_block').connection) ||
       (blockB.type == Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
@@ -322,7 +322,7 @@ Blockly.Connection.prototype.canConnectWithReason_ = function(target) {
     // And hack to fix #1534: Fail attempts to connect anything but a
     // defnoreturn block to a prototype block.
     return Blockly.Connection.REASON_CUSTOM_PROCEDURE;
-  }
+  } */
   return Blockly.Connection.CAN_CONNECT;
 };
 
