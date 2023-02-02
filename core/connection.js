6a3c7b6072495ba812e24cfee4ffb4bb7125ba5d
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-02-01 23:18:16 -0800

    Update connection.js

--- core/connection.js
+++ core/connection.js
@@ -312,12 +312,20 @@ Blockly.Connection.prototype.canConnectWithReason_ = function(target) {
     return Blockly.Connection.REASON_CHECKS_FAILED;
   } else if (blockA.isShadow() && !blockB.isShadow()) {
     return Blockly.Connection.REASON_SHADOW_PARENT;
-  } else if ((((blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE || 
-      blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE + '_return') &&
-      blockB.type != Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
-      superiorConn == blockA.getInput('custom_block').connection) ||
-      (blockB.type == Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
-      blockA.type != Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE))) {
+  } else if ((
+      (
+        (
+          blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE || 
+          blockA.type == Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE + '_return'
+        ) &&
+        blockB.type != Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
+        superiorConn == blockA.getInput('custom_block').connection
+      ) ||
+      (
+        blockB.type == Blockly.PROCEDURES_PROTOTYPE_BLOCK_TYPE &&
+        blockA.type != Blockly.PROCEDURES_DEFINITION_BLOCK_TYPE
+      )
+    )) {
     // Hack to fix #1127: Fail attempts to connect to the custom_block input
     // on a defnoreturn block, unless the connecting block is a specific type.
     // And hack to fix #1534: Fail attempts to connect anything but a
