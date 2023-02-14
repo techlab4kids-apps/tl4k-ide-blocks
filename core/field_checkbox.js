54c8f900d0037b1a7388098e4e127fa2709fcea7
RedMan13 <74981904+RedMan13@users.noreply.github.com>
2023-02-14 01:33:15 -0800

    Update field_checkbox.js

--- core/field_checkbox.js
+++ core/field_checkbox.js
@@ -78,7 +78,7 @@ Blockly.FieldCheckbox.prototype.init = function() {
   // Instead it uses a custom checkmark element that is either visible or not.
   this.checkElement_ = Blockly.utils.createSvgElement(
     'image',
-    {'height': '20px', 'width': '20px', 'x': 0, 'y': 7}, 
+    {'height': '15px', 'width': '20px', 'x': 0, 'y': 7}, 
     this.fieldGroup_
   );
   this.setValue(this.getValue());
