b49dea3e528eb652570d04c9797a05a060138dbe
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2023-08-06 00:09:47 -0700

    why did i not do this for this long

--- blocks_common/colour.js
+++ blocks_common/colour.js
@@ -36,7 +36,7 @@ goog.require('Blockly.constants');
  */
 function randomColour() {
   var num = Math.floor(Math.random() * Math.pow(2, 24));
-  return '#' + ('00000' + num.toString(16)).substr(-6) + 'ff';
+  return '#' + ('00000' + num.toString(16)).substr(-6);
 }
 
 Blockly.Blocks['colour_picker'] = {
