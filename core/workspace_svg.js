6a02099390dd6711d8e8178d396417854c000413
RedMan13 <godslayerakp@gmail.com>
2024-01-18 20:34:47 -0800

    okay cool can you work though? thx

--- core/workspace_svg.js
+++ core/workspace_svg.js
@@ -1017,10 +1017,14 @@ Blockly.WorkspaceSvg.prototype.reportValue = function(id, value, isError) {
   Blockly.DropDownDiv.hideWithoutAnimation();
   Blockly.DropDownDiv.clearContent();
   var contentDiv = Blockly.DropDownDiv.getContentDiv();
-  var valueReportBox = goog.dom.createElement('div');
-  valueReportBox.setAttribute('class', 'valueReportBox');
-  valueReportBox.textContent = value;
-  contentDiv.appendChild(valueReportBox);
+  if (!value.toReporterContent) {
+    var valueReportBox = goog.dom.createElement('div');
+    valueReportBox.setAttribute('class', 'valueReportBox');
+    valueReportBox.textContent = value;
+    contentDiv.appendChild(valueReportBox);
+  } else {
+    contentDiv.appendChild(value.toReporterContent())
+  }
   Blockly.DropDownDiv.setColour(
       Blockly.Colours.valueReportBackground,
       isError 
