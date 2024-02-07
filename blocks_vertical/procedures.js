10e057fde30883f7d6629dbdcf6cff4629341b60
godslayerakp <74981904+RedMan13@users.noreply.github.com>
2024-02-07 02:10:57 -0800

    oh yeah and do that too so you dont die on letters

--- blocks_vertical/procedures.js
+++ blocks_vertical/procedures.js
@@ -305,8 +305,9 @@ Blockly.ScratchBlocks.ProcedureUtils.createAllInputs_ = function(connectionMap) 
   for (var i = 0, component; component = procComponents[i]; i++) {
     var labelText;
     var argumentType = component.substring(1, 2);
+    var id = this.argumentIds_[argumentCount];
     // user error shouldnt literally nuke the app, ignore invalid markers instead of erroring on them
-    if (component.substring(0, 1) == '%' && (argumentType == 'n' || argumentType == 'b' || argumentType == 's')) {
+    if (component.substring(0, 1) == '%' && (argumentType == 'n' || argumentType == 'b' || argumentType == 's') && id) {
       /*
       if (!(argumentType == 'n' || argumentType == 'b' || argumentType == 's')) {
         throw new Error(
@@ -314,8 +315,6 @@ Blockly.ScratchBlocks.ProcedureUtils.createAllInputs_ = function(connectionMap) 
       }
       */
       labelText = component.substring(2).trim();
-
-      var id = this.argumentIds_[argumentCount];
 
       var input = this.appendValueInput(id);
       if (argumentType == 'b') {
